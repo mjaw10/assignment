86f6077113ef3ec888c08bb6a4da2f720903f63e
mjaw10 <40535@longroad.ac.uk>
2014-09-15 12:57:11 +0100

    completed development exercise 1

---
+++ development exercise 1.py
@@ -0,0 +1,5 @@
+num1 = int(input("Please enter an interger: "))
+num2 = int(input("Please enter an interger you want to divide the fist interger by: "))
+ans = num1 // num2
+remainder = num1 % num2
+print("The answer is {0} remainder {1}".format(ans,remainder))
