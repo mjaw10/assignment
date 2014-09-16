28ae7f18e29310b9a9d00f1ebf38879cc07acdcf
mjaw10 <40535@longroad.ac.uk>
2014-09-16 12:38:36 +0100

    made more progress

--- binary to hex.py
+++ binary to hex.py
@@ -3,5 +3,11 @@ bit4 = int(binary_no[0])
 bit3 = int(binary_no[1])
 bit2 = int(binary_no[2])
 bit1 = int(binary_no[3])
+ph8 = bit4 * 8
+ph4 = bit3 * 4
+ph2 = bit2 * 2
+ph1 = bit1 * 1
+decimal_no = ph8 + ph4 + ph2 + ph1
+print(decimal_no)
 
 
