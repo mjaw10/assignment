611592f7f1dc9161a56ef1bb72f50f2197565d10
mjaw10 <40535@longroad.ac.uk>
2014-09-19 13:05:18 +0100

    improved it to go up to 4095

--- decimal to hexidecimal.py
+++ decimal to hexidecimal.py
@@ -2,38 +2,58 @@
 #18/09/14
 #This program will change a number from decimal to hexidecimal
 
-DecimalValue = int(input("Enter an interger between 1 and 255: "))
+DecimalValue = int(input("Enter an interger between 0 and 4095: "))
 BinaryString = ""
 while DecimalValue > 0:
     BinaryString = str(DecimalValue % 2) + BinaryString
     DecimalValue = DecimalValue // 2
+    
 no_of_bits = len(BinaryString)
 
-if no_of_bits == 1:
-    BinaryString = "0000000" + BinaryString
+if no_of_bits == 0:
+    BinaryString = "000000000000" + BinaryString
+elif no_of_bits == 1:
+    BinaryString = "00000000000" + BinaryString
 elif no_of_bits == 2:
-    BinaryString = "000000" + BinaryString
+    BinaryString = "0000000000" + BinaryString
 elif no_of_bits == 3:
-    BinaryString = "00000" + BinaryString
+    BinaryString = "000000000" + BinaryString
 elif no_of_bits == 4:
-    BinaryString = "0000" + BinaryString
+    BinaryString = "00000000" + BinaryString
 elif no_of_bits == 5:
-    BinaryString = "000" + BinaryString
+    BinaryString = "0000000" + BinaryString
 elif no_of_bits == 6:
-    BinaryString = "00" + BinaryString
+    BinaryString = "000000" + BinaryString
 elif no_of_bits == 7:
+    BinaryString = "00000" + BinaryString
+elif no_of_bits == 8:
+    BinaryString = "0000" + BinaryString
+elif no_of_bits == 9:
+    BinaryString = "000" + BinaryString
+elif no_of_bits == 10:
+    BinaryString = "00" + BinaryString
+elif no_of_bits == 11:
     BinaryString = "0" + BinaryString
 else:
     BinaryString = BinaryString
 
-bit8 = int(BinaryString[0])
-bit7 = int(BinaryString[1])
-bit6 = int(BinaryString[2])
-bit5 = int(BinaryString[3])
-bit4 = int(BinaryString[4])
-bit3 = int(BinaryString[5])
-bit2 = int(BinaryString[6])
-bit1 = int(BinaryString[7])
+bit12 = int(BinaryString[0])
+bit11 = int(BinaryString[1])
+bit10 = int(BinaryString[2])
+bit9 = int(BinaryString[3])
+bit8 = int(BinaryString[4])
+bit7 = int(BinaryString[5])
+bit6 = int(BinaryString[6])
+bit5 = int(BinaryString[7])
+bit4 = int(BinaryString[8])
+bit3 = int(BinaryString[9])
+bit2 = int(BinaryString[10])
+bit1 = int(BinaryString[11])
+
+ph8_2 = bit12 * 8
+ph4_2 = bit11 * 4
+ph2_2 = bit10 * 2
+ph1_2 = bit9 * 1
 
 ph8_1 = bit8 * 8
 ph4_1 = bit7 * 4
@@ -45,8 +65,42 @@ ph4 = bit3 * 4
 ph2 = bit2 * 2
 ph1 = bit1 * 1
 
+decimal_no_2 = ph8_2 + ph4_2 + ph2_2 + ph1_2
 decimal_no_1 = ph8_1 + ph4_1 + ph2_1 + ph1_1
 decimal_no = ph8 + ph4 + ph2 + ph1
+
+if decimal_no_2 == 0:
+    hex2 = 0
+elif decimal_no_2 == 1:
+    hex2 = 1
+elif decimal_no_2 == 2:
+    hex2 = 2
+elif decimal_no_2 == 3:
+    hex2 = 3
+elif decimal_no_2 == 4:
+    hex2 = 4
+elif decimal_no_2 == 5:
+    hex2 = 5
+elif decimal_no_2 == 6:
+    hex2 = 6
+elif decimal_no_2 == 7:
+    hex2 = 7
+elif decimal_no_2 == 8:
+    hex2 = 8
+elif decimal_no_2 == 9:
+    hex2 = 9
+elif decimal_no_2 == 10:
+    hex2 = "A"
+elif decimal_no_2 == 11:
+    hex2 = "B"
+elif decimal_no_2 == 12:
+    hex2 = "C"
+elif decimal_no_2 == 13:
+    hex2 = "D"
+elif decimal_no_2 == 14:
+    hex2 = "E"
+else:
+    hex2 = "F"
 
 if decimal_no_1 == 0:
     hex1 = 0
@@ -114,4 +168,4 @@ elif decimal_no == 14:
 else:
     hex0 = "F"
 
-print("Your number in hexidecimal is {0}{1}".format(hex1,hex0))
+print("Your number in hexidecimal is {0}{1}{2}".format(hex2,hex1,hex0))
